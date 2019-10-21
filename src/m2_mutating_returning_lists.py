db80f2be03cb55c3e064235d66f91de738c3b28e
DavidMutchler <mutchler@rose-hulman.edu>
2019-10-21 16:39:16 -0400

    Moved the zero_changer problem from the former m1 to this module.

--- src/m2_mutating_returning_lists.py
+++ src/m2_mutating_returning_lists.py
@@ -113,6 +113,65 @@ def doubler(list1, list2):
     # -------------------------------------------------------------------------
 
 
+def run_test_zero_changer():
+    """ Tests the    zero_changer    function. """
+    print()
+    print('--------------------------------------------------')
+    print('Testing the   zero_changer   function:')
+    print('--------------------------------------------------')
+
+    # Test 1:
+    test1 = ([8, 4, 0, 9], [77, 0, 0, 1, 5, 0], [4, 4, 4], [4, 0, 4])
+    expected1 = ([8, 4, 1, 9], [77, 2, 3, 1, 5, 4], [4, 4, 4], [4, 5, 4])
+    zero_changer(test1)
+    print()
+    print('Test 1:')
+    print('  Expected:', expected1)
+    print('  Actual:  ', test1)
+
+    # -------------------------------------------------------------------------
+    # TODO: 2. Write at least 2 additional tests for the
+    #    zero_changer
+    # function.  Try to choose some unexpected things like empty lists
+    # or an empty tuple, or a list with no zeros, etc.
+    # -------------------------------------------------------------------------
+
+
+def zero_changer(tuple_of_lists):
+    """
+    What comes in:  A TUPLE of LISTs,
+                    where the interior lists contain only integers.
+    What goes out:  Nothing (i.e., none)
+    Side effects:  The argument is MUTATED so that:
+      -- the 1st 0 in the given tuple of lists is changed to 1.
+      -- the 2nd 0 in the given tuple of lists is changed to 2.
+      -- the 3rd 0 in the given tuple of lists is changed to 3.
+           etc.
+    Example:
+      If the given tuple of lists is:
+          ([8, 4, 0, 9], [77, 0, 0, 1, 5, 0], [4, 4, 4], [4, 0, 4])
+      then AFTER this function is called with that tuple of lists,
+      the tuple of lists has been MUTATED to:
+          ([8, 4, 1, 9], [77, 2, 3, 1, 5, 4], [4, 4, 4], [4, 5, 4])
+    Note that:
+      -- If there are no zeros in the given tuple of lists,
+           then this function does nothing.
+      -- After this function call, the tuple of lists IN THE CALLER
+           should contain no zeros.
+    Type hints:
+      :type tuple_of_lists: tuple of list[int]
+    """
+    # -------------------------------------------------------------------------
+    # TODO: 3. Implement and test this function.
+    #   Note that you should write its TEST function first (above).
+    # -------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
+    #    DIFFICULTY:      7
+    #    TIME ESTIMATE:  10 minutes.
+    # -------------------------------------------------------------------------
+
+
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
