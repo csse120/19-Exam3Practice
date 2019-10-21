d07fb89f0e3cb8b91002b755b1f62a98d3cad1d8
DavidMutchler <mutchler@rose-hulman.edu>
2019-10-21 19:20:32 -0400

    Standardized message re difficulty ratings et al.

--- src/m4_loops.py
+++ src/m4_loops.py
@@ -11,23 +11,26 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# Students:
-#
-# These problems have DIFFICULTY and TIME ratings:
-#  DIFFICULTY rating:  1 to 10, where:
-#     1 is very easy
-#     3 is an "easy" Test 2 question.
-#     5 is a "typical" Test 2 question.
-#     7 is a "hard" Test 2 question.
-#    10 is an EXTREMELY hard problem (too hard for a Test 2 question)
-#
-#  TIME ratings: A ROUGH estimate of the number of minutes that we
-#     would expect a well-prepared student to take on the problem.
-#
-#  IMPORTANT: For ALL the problems in this module,
-#    if you reach the time estimate and are NOT close to a solution,
-#    STOP working on that problem and ASK YOUR INSTRUCTOR FOR HELP
-#    on it, in class or via Piazza.
+# TODO: 2.  [Note: same _TODO_ as its matching one in module m1.]
+#  Students:
+#  __
+#  These problems have DIFFICULTY and TIME ratings:
+#    DIFFICULTY rating:  1 to 10, where:
+#       1 is very easy
+#       3 is an "easy" Exam 3 question.
+#       5 is a "typical" Exam 3 question.
+#       7 is a "hard" Exam 3 question.
+#      10 is an EXTREMELY hard problem (too hard for a Exam 3 question)
+#  __
+#    TIME ratings: A ROUGH estimate of the number of minutes that we
+#       would expect a well-prepared student to take on the problem.
+#  __
+#    IMPORTANT: For ALL the problems in this module,
+#      if you reach the time estimate and are NOT close to a solution,
+#      STOP working on that problem and ASK YOUR INSTRUCTOR FOR HELP on it,
+#      in class or via Piazza.
+#  __
+#  After you read (and understand) the above, change this _TODO_ to DONE.
 ###############################################################################
 
 import simple_testing as st
