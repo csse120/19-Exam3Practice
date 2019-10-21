786a358a40026c3da241195639c7f8c83616c22b
DavidMutchler <mutchler@rose-hulman.edu>
2019-10-21 16:51:52 -0400

    Added a trivial problem on console input just so it does not get left out from this set of review problems.

---
+++ src/m3_console_input.py
@@ -0,0 +1,53 @@
+"""
+PRACTICE Exam 3.
+
+This problem provides practice at: *** INPUT from the CONSOLE. ***
+
+Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
+         Mark Hays, Amanda Stouder, Derek Whitley, their colleagues,
+         and PUT_YOUR_NAME_HERE.
+"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
+
+def main():
+    """ TESTs the functions in this module (by calling them). """
+    input_it_all()
+
+
+def input_it_all():
+    """
+    What comes in: Nothing.
+    What goes out: Returns a string as described in Side effects (below).
+    Side effects:
+      Prompts the user for and inputs:
+        -- A positive floating point number
+        -- A positive integer
+        -- A string
+      in that order (via three separate INPUT statements).
+      Then computes the sum of the integer and floating point number,
+      converts that result to a string, and returns the string that is
+      the INPUT string followed by the COMPUTED string.
+      No input validation is required.
+
+    Example:
+    Here is a sample run, where the user input is to the right
+    of the colons:
+         Enter a positive floating point number: 1.44
+         Enter a positive integer: 4
+         Enter a string: Peace & Love.
+
+         Returns Peace & Love5.44
+    """
+    # -------------------------------------------------------------------------
+    # TODO: 2. Implement and test this function.
+    #   The testing code is already written for you (above).
+    #  __
+    #  HINT: If you add an  import math   statement, put it at the BEGINNING
+    #        of this module, NOT ** INSIDE ** the definition of this function.
+    # -------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# Calls  main  to start the ball rolling.
+# -----------------------------------------------------------------------------
+main()
